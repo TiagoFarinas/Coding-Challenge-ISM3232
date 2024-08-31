ccea3e26d6ef579fc486bce8d33498e5657611b9
TiagoFarinas <tiagofarinas@gmail.com>
2024-08-30 22:18:26 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,12 @@ var isActive= true;
 console.log(employeeName);
 console.log(employeeID);
 console.log(isActive)
+
+//Task 2
+let products = ["Desk", "Chair", "Bed"]
+const productDetails = {
+name: John,
+price: 500.00,
+inStock: yes
+}
+console.log(products, productDetails)
